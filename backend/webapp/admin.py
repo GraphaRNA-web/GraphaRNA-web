4f199c9315f0386a36af0f8db4fc354125ba2ae9
fifi2323 <filipurbanski23@gmail.com>
2025-07-13 19:26:41 +0200

    registered models

--- backend/webapp/admin.py
+++ backend/webapp/admin.py
@@ -1,3 +1,13 @@
-#from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Job, JobResults
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('job_name', 'email', 'created_at', 'expires_at')
+    search_fields = ('job_name', 'email')
+
+@admin.register(JobResults)
+class JobResultsAdmin(admin.ModelAdmin):
+    list_display = ('job', 'completed_at', 'result_structure')
+    search_fields = ('result_structure',)
