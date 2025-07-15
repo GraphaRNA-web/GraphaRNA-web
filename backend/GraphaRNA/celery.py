a94141d6a9dfffa5b27d7595d0fa0ca5d8ac1dd0
fifi2323 <filipurbanski23@gmail.com>
2025-07-15 11:47:17 +0200

    Scheduled celery beat task for auto-deletion of expired jobs, changed timezone

---
+++ backend/GraphaRNA/celery.py
@@ -0,0 +1,21 @@
+from celery import Celery
+from celery.schedules import crontab
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GraphaRNA.settings')
+
+
+app = Celery('GraphaRNA')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+
+app.conf.beat_schedule = {
+    'delete-expired-jobs-every-day': {
+        'task': 'webapp.tasks.delete_expired_jobs',
+        'schedule': crontab(hour='*/1', minute=0), #Run the task every hour eg. 0.00, 1.00, ...
+    },
+}
+
+
+
