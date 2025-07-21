cbb557c2fb0e613482580e2f0d9095bec3991a5a
unknown <tymek9.pawlowski@gmail.com>
2025-07-21 22:29:15 +0200

    RNA API, validations for emails and seed. Seed generator

--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -1,3 +1,23 @@
 # from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect, HttpResponseRedirect
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from rest_framework import status
+
+
+
+
+@api_view(['GET'])
+def hello(request):
+    name = request.GET.get('name', 'guest')
+    data = {
+        'name': name,
+        'message': f"Hello {name}"
+    }
+    return Response(data, status=status.HTTP_200_OK)
+
+
+
+
 
 # Create your views here.
