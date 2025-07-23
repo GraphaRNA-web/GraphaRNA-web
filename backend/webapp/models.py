0fbbf99d2de11562dd6d43791a3032ef8c251a07
fifi2323 <filipurbanski23@gmail.com>
2025-07-23 23:33:39 +0200

    added status field to job model

--- backend/webapp/models.py
+++ backend/webapp/models.py
@@ -10,6 +10,11 @@ def default_expiration() -> datetime:
 
 
 class Job(models.Model):
+    STATUS = {
+        'Q': 'Queued',
+        'P': 'Processing',
+        'F': 'Finished'
+    }
     uid: models.UUIDField = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False
     )
@@ -19,7 +24,7 @@ class Job(models.Model):
     email: models.CharField = models.CharField(max_length=255, null=True, blank=True)
     created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
     expires_at: models.DateTimeField = models.DateTimeField(default=default_expiration)
-
+    status: models.TextField = models.TextField(choices=STATUS)
     def __str__(self) -> str:
         return str(self.job_name)
 
