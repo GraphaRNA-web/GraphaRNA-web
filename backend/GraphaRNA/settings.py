e07851f6dd658fbd806131333a5f913f4ecf59fd
Filip Urbański <147380581+fifi2323@users.noreply.github.com>
2025-09-16 17:00:17 +0200

    Update settings.py

--- backend/GraphaRNA/settings.py
+++ backend/GraphaRNA/settings.py
@@ -37,7 +37,7 @@ ENGINE_URL = os.getenv("ENGINE_URL", "http://grapharna-engine:8080/run")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
