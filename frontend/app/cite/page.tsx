e3e6ba5a392ce352e344b0aa1b365b87ca2d1fd3
Bartek Skrzypczak <b.skrzypczak21@gmail.com>
2025-09-29 19:02:28 +0200

    Cite Us page

--- frontend/app/cite/page.tsx
+++ frontend/app/cite/page.tsx
@@ -1,5 +1,20 @@
+import '../styles/cite.css';
+
 export default function CiteUs(){
     return(
-        <h1>Cite Us</h1>
+        <div className='content'>
+            <div className='cite'>
+                <p className='cite-title'>Cite us</p>
+                <div className='cite-content'>
+                    <p className='cite-subtitle'>Any published work which has made use of GraphaRNA should cite the following paper:</p>
+                    <img className='circle' src='photos/cp_circle.png'></img>
+                    <img className='triangle' src='photos/cp_triangle.png'></img>
+                    <img className='green-thing' src='icons/cp_thing.svg'></img>
+                    <div className='text'>
+                        <p className='para'>Justyna M, Zirbel CL, Antczak M, Szachniuk M (2025), Graph Neural Network and Diffusion Model for modeling RNA interatomic interactions, submitted</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
