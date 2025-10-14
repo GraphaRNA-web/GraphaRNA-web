26e43bb13a05e735c9e65e85369ebde75daa235f
Bartek <b.skrzypczak21@gmail.com>
2025-10-14 19:09:56 +0200

    Zmiana nazwy strony na submitJob

--- frontend/app/jobs/page.tsx
+++ frontend/app/jobs/page.tsx
@@ -1,20 +1,7 @@
-import '../styles/cite.css';
-
-export default function CiteUs(){
+export default function JobsPage(){
     return(
-        <div className='content'>
-            <div className='cite'>
-                <p className='cite-title'>Cite us</p>
-                <div className='cite-content'>
-                    <p className='cite-subtitle'>Any published work which has made use of GraphaRNA should cite the following paper:</p>
-                    <img className='circle' src='photos/cp_circle.png'></img>
-                    <img className='triangle' src='photos/cp_triangle.png'></img>
-                    <img className='green-thing' src='icons/cp_thing.svg'></img>
-                    <div className='text'>
-                        <p className='para'>Justyna M, Zirbel CL, Antczak M, Szachniuk M (2025), Graph Neural Network and Diffusion Model for modeling RNA interatomic interactions, submitted</p>
-                    </div>
-                </div>
-            </div>
+        <div className='jp-content'>
+            {/* TODO */}
         </div>
     )
 }
