8640f18e3fedc06bbe071d0028bf95f1cc0c2700
fifi2323 <filipurbanski23@gmail.com>
2025-10-25 20:18:53 +0200

    more tests, add slight diffrence allowance in screenshots

--- playwright/elements/visualElement.ts
+++ playwright/elements/visualElement.ts
@@ -1,4 +1,4 @@
-import { Page, expect } from "@playwright/test";
+import { Expect, Locator, LocatorScreenshotOptions, Page, expect } from "@playwright/test";
 import { BaseElement } from "./baseElement";
 
 export class VisualElement extends BaseElement {
@@ -6,12 +6,15 @@ export class VisualElement extends BaseElement {
     super(selector, page);
   }
 
-  
-  async toHaveScreenshot(name: string, options?: Parameters<typeof expect['extend']>[0]) {
-    await expect(this.element()).toHaveScreenshot(name, options);
-  }
 
-  
+   async toHaveScreenshot(
+      name: string,
+      options?: Parameters<ReturnType<typeof expect>['toHaveScreenshot']>[1]
+    ) {
+      await expect(this.element()).toHaveScreenshot(name, options);
+    }
+
+
   async screenshot(path: string, options?: { fullPage?: boolean }) {
     await this.element().screenshot({ path, ...options });
   }
