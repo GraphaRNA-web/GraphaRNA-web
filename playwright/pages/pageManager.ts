6984f820bd3664df25cb0bb7300f4ad514c94e3a
Pwlklr <pawelkelar@outlook.com>
2025-10-26 14:58:29 +0100

    Dodanie testow dla statycznych stron

--- playwright/pages/pageManager.ts
+++ playwright/pages/pageManager.ts
@@ -1,10 +1,18 @@
 import { Page } from "@playwright/test"
 import { NavigationPage } from "./navigationPage";
+import { HomePage } from "./homePage";
+import { AboutPage } from "./aboutPage";
+import { CitePage } from "./citeUsPage";
+import { GuidePage } from "./guidePage";
 
 export class PageManager {
     private readonly page: Page;
  //   private itemsPage: ItemsPage;
     private navigationPage: NavigationPage;
+    private homePage: HomePage;
+    private aboutPage: AboutPage;
+    private citePage: CitePage;
+    private guidePage: GuidePage;
 
 
     constructor(page: Page) {
@@ -17,6 +25,22 @@ export class PageManager {
     // }
     get onNavigationPage() {
         return this.navigationPage ?? (this.navigationPage = new NavigationPage(this.page));
+    }
+
+    get onHomePage() {
+        return this.homePage ?? (this.homePage = new HomePage(this.page));
+    }
+
+    get onAboutPage() {
+        return this.aboutPage ?? (this.aboutPage = new AboutPage(this.page));
+    }
+
+    get onCitePage() {
+        return this.citePage ?? (this.citePage = new CitePage(this.page));
+    }
+
+    get onGuidePage() {
+        return this.guidePage ?? (this.guidePage = new GuidePage(this.page));
     }
 
 
