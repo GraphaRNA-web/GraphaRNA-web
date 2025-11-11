7d861ccbcb9087cad912ced64b237f9f9c1174fd
AzuRRoo <tymek9.pawlowski@gmail.com>
2025-11-11 18:43:02 +0100

    divided tests to different files

--- playwright/tests/submitJob_validation.spec.ts
+++ playwright/tests/submitJob_validation.spec.ts
@@ -68,79 +68,30 @@ test.describe("SubmitJob RNA Validation Example 3", () => {
 });
 
 
-test.describe("SubmitJob interactive validation error", () => {
-  test("Should switch from text to Interactive format and succesfully validate", async ({ page }) => {
+test.describe("SubmitJob invalid DotBRacket", () => {
+  test("Should show inalid DotBracket", async ({ page }) => {
     const submitJob = new SubmitJobPage(page);
 
     await submitJob.goto();
-    await expect(submitJob.textArea).toBeVisible();
-    await submitJob.selectInputFormat("Interactive");
-    await expect(submitJob.interactiveBox).toBeVisible();
-    await submitJob.fillStructure(0, "CC\n..");
-    await submitJob.addStructure();
-    await submitJob.fillStructure(1, "GG\n..");
-
-    const firstVal = await submitJob.interactiveBox.locator("textarea").nth(0).inputValue();
-    const secondVal = await submitJob.interactiveBox.locator("textarea").nth(1).inputValue();
-
-    expect(firstVal).toBe("CC\n..");
-    expect(secondVal).toBe("GG\n..");
-
+    await submitJob.fillTextArea("GG\n&)");
     await submitJob.clickValidate();
-    await expect(submitJob.approveBox).toBeVisible({ timeout: 15000 });
-    await expect(submitJob.approveBox).toHaveText("Validation passed successfully. Input was parsed to the engine's format.",{ timeout: 15000 });
 
-  });
-
-});
-
-
-
-test.describe("SubmitJob interactive blank 1 ", () => {
-  test("Should switch from Text to Interactive format and get validation error blank Structure 1 ", async ({ page }) => {
-    const submitJob = new SubmitJobPage(page);
-
-    await submitJob.goto();
-    await expect(submitJob.textArea).toBeVisible();
-    await submitJob.selectInputFormat("Interactive");
-    await expect(submitJob.interactiveBox).toBeVisible();
-    await submitJob.fillStructure(0, "");
-    await submitJob.addStructure();
-    await submitJob.fillStructure(1, "GG\n..");
-
-    const firstVal = await submitJob.interactiveBox.locator("textarea").nth(0).inputValue();
-    const secondVal = await submitJob.interactiveBox.locator("textarea").nth(1).inputValue();
-
-    expect(firstVal).toBe("");
-    expect(secondVal).toBe("GG\n..");
-
-    await submitJob.clickValidate();
     await expect(submitJob.errorBox).toBeVisible();
-    await expect(submitJob.errorBox).toContainText("Structure 1 cannot be empty.",{ timeout: 15000 });
+    await expect(submitJob.errorBox).toContainText("DotBracket contains invalid brackets: &",{ timeout: 15000 });
   });
 });
 
-
-test.describe("SubmitJob interactive blank 2", () => {
-  test("Should switch from Text to Interactive format and get validation error blank Structure 2", async ({ page }) => {
+test.describe("SubmitJob invalid RNA", () => {
+  test("Should show inalid RNA", async ({ page }) => {
     const submitJob = new SubmitJobPage(page);
 
     await submitJob.goto();
-    await expect(submitJob.textArea).toBeVisible();
-    await submitJob.selectInputFormat("Interactive");
-    await expect(submitJob.interactiveBox).toBeVisible();
-    await submitJob.fillStructure(0, "CC\n..");
-    await submitJob.addStructure();
-    await submitJob.fillStructure(1, "");
-
-    const firstVal = await submitJob.interactiveBox.locator("textarea").nth(0).inputValue();
-    const secondVal = await submitJob.interactiveBox.locator("textarea").nth(1).inputValue();
-
-    expect(firstVal).toBe("CC\n..");
-    expect(secondVal).toBe("");
-
+    await submitJob.fillTextArea("GL\n..");
     await submitJob.clickValidate();
+
     await expect(submitJob.errorBox).toBeVisible();
-    await expect(submitJob.errorBox).toContainText("Structure 2 cannot be empty.",{ timeout: 15000 });
+    await expect(submitJob.errorBox).toContainText("RNA contains invalid characters: L",{ timeout: 15000 });
   });
 });
+
+
