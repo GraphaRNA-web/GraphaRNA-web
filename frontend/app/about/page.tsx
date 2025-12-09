aba8758540737e674ca2d3d42641323ecaeb1840
Bartek Skrzypczak <b.skrzypczak21@gmail.com>
2025-12-09 16:07:37 +0100

    FE - drobne zmiany frontendowe Fixes #209

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -9,28 +9,21 @@ export default function About(){
                     <img className='circle-top' src='photos/ap_circle_top.png'></img>
                     <img className='triangle-top' src='photos/ap_triangle_top.png'></img>
                     <div className='about-text'>
-                        <p className='about-p1'>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-                            Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-                            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
-                            Cras dapibus. Vivamus elementum semper nisi. 
+                        <p className='about-p'>
+                            GraphaRNA is an easy-to-use web application for generating 3D RNA structures from sequence 
+                            and secondary structure input. Powered by graph neural networks and diffusion modeling, 
+                            it learns interatomic interactions and predicts realistic RNA conformations with atomic-level 
+                            detail.
                         </p>
-                        <p className='about-p2'>
-                            Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-                            Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus 
-                            varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur 
-                            ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum 
-                            rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel,
-                            luctus pulvinar, hendrerit id, lorem. 
+                        <p className='about-p'>
+                            Designed for flexibility and exploration, GraphaRNA allows users to model small RNA 
+                            motifs, test structural hypotheses, and examine alternative base-pairing scenarios -- all 
+                            through a fast and intuitive interface, without installing any software.
                         </p>
-                        <p className='about-p3'>
-                            Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. 
-                            Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus 
-                            et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu.
-                            Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris.
-                            Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. 
+                        <p className='about-p'>
+                            The source code of 
+                            the GraphaRNA computational engine is available on <a href="https://github.com/mjustynaPhD/GraphaRNA" className="about-link">GitHub</a>, 
+                            and the training/test datasets together with pre-trained model weights are provided on <a href="https://zenodo.org/records/13750967" className="about-link"> Zenodo</a>.
                         </p>
                     </div>
                 </div>
@@ -115,7 +108,9 @@ export default function About(){
                 <div className='funds-content'>
                     <p className='funds-title'>Acknowledgements and Funding</p>
                     <p className='funds-text'>
-                        This research was supported by grants 2020/39/O/ST6/01488, 2023/51/D/ST6/01207 from the National Science Centre, Poland.
+                        This project was supported by the National Science Centre, Poland 
+                        (grants 2020/39/O/ST6/01488 and 2023/51/D/ST6/01207), and by the statutory funds 
+                        of Poznan University of Technology.
                     </p>
                     <img className='triangle-bottom' src='photos/ap_triangle_bot.png'></img>
                 </div>
