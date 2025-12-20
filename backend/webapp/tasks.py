ec135c6fbf900331af7c7d3a24606f58a333cfa0
fifi2323 <filipurbanski23@gmail.com>
2025-12-20 21:40:11 +0100

    fix infinite loop when polling engine and recive no response

--- backend/webapp/tasks.py
+++ backend/webapp/tasks.py
@@ -152,6 +152,7 @@ def execute_and_poll_engine(
     status_url = f"{engine_url}/status/{uuid}"
     cancel_url = f"{engine_url}/cancel/{uuid}"
 
+    retries = 0
     while True:
         logger.info("Polling engine for results...")
         if time() - start_time > timeout:
@@ -167,7 +168,10 @@ def execute_and_poll_engine(
             status_resp = requests.get(f"{status_url}?seed={seed}")
         except requests.RequestException:
             logger.warning("Failed to get status from engine, retrying...")
+            retries += 1
             sleep(check_interval)
+            if retries >= settings.ENGINE_REQUEST_MAX_RETRIES:
+                raise Exception("Max retries reached while polling engine status")
             continue
 
         if status_resp.status_code == 200:
